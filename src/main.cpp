40da41919a07e30992684ba2efe6db9246465562
Electron77 <teahaile2@teahaile.com>
2025-07-21 03:13:41 -0400

    feature(prototype for colored output) TODO: Change this to be a function that takes an enum (or just use multiple macros if that's easier to use with the native 'printf' function).

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "common.hpp"
 #include <cstdio>
 
+// TODO: Put this macro (and more) in its own header file (or make it a function instead, that takes an enum for the color type)
+#include <string> // IWYU pragma: keep
+#define COLOR_BLUE(text) "\033[1;34m " text " \033[0m"
+
 int PrintHelp()
 {
 	printf("%s", HELP_STRING);
 #ifdef DEBUGGING
-	printf("\n    knave %s-debug\n", VERSION_STRING);
+	printf(COLOR_BLUE("\n    knave %s-debug\n"), VERSION_STRING);
 #else
 	printf("\n    knave %s\n", VERSION_STRING);
 #endif
